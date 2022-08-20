773ea63f2fdde6ee21229d3eefd47d5733537a25
abajit <abajit@mail.com>
2022-08-20 08:33:30 +0530

    Added animation

--- src/index.js
+++ src/index.js
@@ -1,89 +1,46 @@
-const userData = [
-  {
-    designerName: "Linda Wilson",
-    primary: "#CECDCE",
-    accent: "#D9C68E",
-    textColor: "#FFFFFF",
-    avatar: "/assets/avatar/avatar-1.jpg",
-    image: "assets/images/furniture-1.jpg",
-    imageDescription: "",
-  },
-  {
-    designerName: "Michael Jones",
-    primary: "#FFFFFF",
-    accent: "#FFB664",
-    textColor: "#010001",
-    avatar: "/assets/avatar/avatar-2.jpg",
-    image: "assets/images/furniture-2.jpg",
-    imageDescription: "",
-  },
-  {
-    designerName: "Thomas Davis",
-    primary: "#CFCECF",
-    accent: "#BBD49B",
-    textColor: "#FFFFFF",
-    avatar: "/assets/avatar/avatar-3.jpg",
-    image: "assets/images/furniture-3.jpg",
-    imageDescription: "",
-  },
-  {
-    designerName: "Patricia Jones",
-    primary: "#B5B8C3",
-    accent: "#E0E5E7",
-    textColor: "#FFFFFF",
-    avatar: "/assets/avatar/avatar-4.jpg",
-    image: "assets/images/furniture-4.jpg",
-    imageDescription: "",
-  },
-];
-
-window.onload = (e) => {
-  changeCarouselItem();
-};
-
-let index = 0;
-const accentDiv = document.getElementById("accent-div");
-const container = document.getElementById("container");
-const nameHeader = document.querySelector(".designer-name");
+const tl = gsap.timeline();
+const designerInfo = document.querySelector(".designer-info");
+const cityLocation = document.querySelector(".location");
+const nav = document.getElementsByClassName("nav");
 const carouselCounter = document.querySelector(".carousel-counter");
-const avatarContainer = document.querySelector('.avatar-container');
-const pictureContainer = document.querySelector('.picture-container');
+const [navOne, navTwo] = nav;
 
-const changeCarouselItem = () => {
-  const data = userData[index];
-  setBackground(data?.primary, data?.accent);
-  setDesignerName(data?.designerName, data?.textColor);
-  setCarouselIndex(index, userData.length, data?.textColor);
-  if (index === userData.length - 1) {
-    index = 0;
-  } else {
-    index++;
+document.addEventListener("readystatechange", (e) => {
+  if (document.readyState === "complete") {
+    animateElements();
   }
-};
+});
 
-const setBackground = (primary, accent) => {
-  container.style.backgroundColor = primary;
-  accentDiv.style.backgroundColor = accent;
-};
+// window.onload = () => {
+//   animateElements();
+// }
 
-const setDesignerName = (name, color) => {
-  name = name.replace(' ', '<br>');
-  nameHeader.innerHTML = name;
-  nameHeader.style.color = color;
-};
-
-const setCarouselIndex = (index, total, color) => {
-  const currentItem = carouselCounter.querySelector('.current-item');
-  const totalItems = carouselCounter.querySelector('.total-items');
-  const line = carouselCounter.querySelector('.hr');
-  currentItem.textContent = `0${index + 1}`;
-  totalItems.textContent = `0${total}`;
-  currentItem.style.color = color;
-  totalItems.style.color = color;
-  line.style.backgroundColor = color;
+function animateElements() {
+  tl.from('.logo', { y: -50, opacity: 0, duration: 0.3});
+  tl.from(navOne.children, {
+    y: -50,
+    opacity: 0,
+    duration: 0.3,
+    stagger: 0.2,
+  })
+  tl.from(navTwo.children, { y: -50, opacity: 0, stagger: 0.2 })
+  tl.from('.menu', { y: -50, opacity: 0, duration: 0.3}, 1.5);
+  tl.from(
+    ".picture-container",
+    {
+      x: 200,
+      opacity: 0,
+      duration: 0.7,
+    },
+    1.7
+  );
+  tl.from(designerInfo.children, {
+    x: -200,
+    opacity: 0,
+    duration: 0.7,
+    stagger: 0.2,
+  }, 1.7);
+  tl.from(cityLocation.children, { x: -20, opacity: 0, duration: 0.7, stagger: 0.2 });
+  tl.from(carouselCounter.children, { y: 100, opacity: 0, duration: 0.7, stagger: 0.2}, 2.5);
+  console.log(tl.duration());
 }
-
-const setImage = (avatar, image) => {
-  const avatarImage = avatarContainer.querySelector('img');
-}
-setInterval(changeCarouselItem, 2000);
